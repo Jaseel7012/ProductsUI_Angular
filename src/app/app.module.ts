efcb288ebf22f51c8275c1f0827a6b2f6e15ca78
jaseel <jaseeljctcs@gmail.com>
2022-12-07 20:05:04 +0530

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ProductsAddComponent } from './products-add/products-add.component';
+import { ViewProductComponent } from './view-product/view-product.component';
+const myroute:Routes=[
+  {
+    path:"",
+    component:ProductsAddComponent
+  },
+  {
+    path:"view",
+    component:ViewProductComponent
+  }
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsAddComponent,
+    ViewProductComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myroute)
   ],
   providers: [],
   bootstrap: [AppComponent]
